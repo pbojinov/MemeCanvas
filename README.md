34bc715b929d3f9b490c97018f6f93685b133b92
Petar <petarbojinov@gmail.com>
2013-07-01 22:19:40 -0700

    groundfloor base css, centered text, add todo

--- README.md
+++ README.md
@@ -2,6 +2,28 @@
 
 Meme generator built using cutting edge HTML5 and angular.js framework...sorry jQuery and IE :(
 
+## Todo
+
+* ~~center text when writing~~
+
+* reduce font when typing more words
+
+* multi line text
+
+* save canvas image to buffer
+
+* ng-upload
+
+* brand canvas with site url @ bottom (like 9gag)
+
+* save locally (name generation = scramble randomCharacters + date? )
+
+* upload to imgur
+
+* recent uploaded  (pull latest X from imgur account)
+
+* Move text around
+
 ###Author
 
 Petar Bojinov
--- app/js/factories.js
+++ app/js/factories.js
@@ -19,37 +19,68 @@ angular.module('myApp.factories', []).
                     canvasHeight = canvas.height,
                     centerWidth = canvasWidth / 2,
                     centerHeight = canvasHeight / 2,
+                    centeredWidthTop,
+                    centeredWidthBottom,
                     topX = 0,
                     topY = 0,
                     bottomX = 0,
                     bottomY = 0,
                     fontStyle = '',
-                    fontSize = ' 35px',
+                    fontSize = [' 50px', ' 45px', ' 35px', ' 30px', ' 20px', ' 16px', ' 10px'],
+                    fontSizeTemp,
+                    fontSizeIndex = 0,
                     fontWeight = ' bold',
                     fontFamily = ' Impact',
                     fontColor = '#FFFFFF',
                     strokeColor = '#000000',
-                    textMetrics;
+                    textMetrics,
+                    topTokens = [],
+                    bottomTokens = [];
+
+                topTokens = top.split(' ');
+                bottomTokens = bottom.split(' ');
 
                 ctx.clearRect(0, 0, canvasWidth, canvasHeight);
                 //ctx.fillRect(0, 0, top, bottom);
 
-                ctx.font = fontStyle + fontWeight + fontSize + fontFamily; //'bold 12px sans-serif';
+                ctx.font = fontStyle + fontWeight + fontSize[fontSizeIndex] + fontFamily; //'bold 12px sans-serif';
                 ctx.textAlign = 'middle';
                 ctx.textBaseline = 'top';
                 ctx.fillStyle = fontColor;
                 ctx.strokeStyle = strokeColor;
                 ctx.lineWidth = 2;
                 bottomY = canvasHeight - parseInt(fontSize, 10);
-                console.log(bottomY);
+                //console.log(bottomY);
 
                 //use fillText before strokeText
-                ctx.fillText(top, centerWidth, 0);
-                ctx.strokeText(top, centerWidth, 0);
+                textMetrics = ctx.measureText(top);
+                centeredWidthTop = (canvasWidth - textMetrics.width) / 2;
+                console.log(textMetrics);
+
+                textMetrics = ctx.measureText(bottom);
+                centeredWidthBottom = (canvasWidth - textMetrics.width) / 2;
+                console.log(textMetrics);
+
+                /*
+                if (textMetrics > canvasWidth) {
+                    fontSizeTemp = fontSizeIndex--;
+                    ctx.font = fontStyle + fontWeight + fontSize[fontSizeTemp] + fontFamily;
+                    console.log(ctx.font);
+                }
+                else {
+                    fontSizeTemp = fontSizeIndex++;
+                    ctx.font = fontStyle + fontWeight + fontSize[fontSizeTemp] + fontFamily;
+                    console.log(ctx.font);
+                    console.log((canvasWidth - textMetrics.width) / 2);
+                }
+                */
+
+                ctx.fillText(top, centeredWidthTop, 10);
+                ctx.strokeText(top, centeredWidthTop, 0);
 
                 //bottom text
-                ctx.fillText(bottom, centerWidth, bottomY - 10);
-                ctx.strokeText(bottom, centerWidth, bottomY - 10);
+                ctx.fillText(bottom, centeredWidthBottom, bottomY - 10);
+                ctx.strokeText(bottom, centeredWidthBottom, bottomY - 10);
             }
         };
         return textRender;
